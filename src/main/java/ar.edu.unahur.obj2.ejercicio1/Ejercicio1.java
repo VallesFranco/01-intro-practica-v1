621d68182ac8197b03f6fdbbc7233c5bcb1ca603
VallesFranco <vallesfrancodavid@gmail.com>
2020-04-07 23:46:29 -0300

    Ejercicio 1 realizado, ejercicio 2 en proceso

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
@@ -2,7 +2,13 @@ package ar.edu.unahur.obj2.ejercicio1;
 
 public class Ejercicio1 {
 
-    public int[] ordenar() {
-        return new int[] {};
+    public static void main(String[] args) {
+
+        int[] v = {1,2,3,4,5,6,7,8,9,10};
+
+        for ( int i=9; i>=0; i--) {
+            System.out.print(v[i] + " ");
+        }
     }
+
 }
---
+++ src/main/java/ar/edu/unahur/obj2/ejercicio2/Ejercicio2.java
@@ -0,0 +1,22 @@
+package ar.edu.unahur.obj2.ejercicio2;
+
+
+import java.util.ArrayList;
+
+public class Ejercicio2 {
+
+    int nro = 15;
+    boolean esMayorQue;
+    List<int> nros = new ArrayList<>(3,2,9,1,4,8,5,10,6,11,7,12,20,15,17,13,14,16,18,19);
+
+
+    public static void Main(String[] nros) {
+
+        for(int i=0; i<nros.length; i++) {
+
+        }
+    }
+    public static void compararNros(nros nro)  {
+
+    }
+}
